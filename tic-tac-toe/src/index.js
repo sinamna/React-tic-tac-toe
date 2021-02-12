9c894f51007911724c9bb9bf5b51c95174a6dd28
sinamna <sina3050@gmail.com>
2021-02-12 11:32:03 +0330

    created index and added alert

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -4,7 +4,7 @@ import './index.css'
 class Square extends React.Component {
     render() {
       return (
-        <button className="square">
+        <button className="square" onClick={()=> alert('clicked')}>
           {this.props.value}
         </button>
       );
