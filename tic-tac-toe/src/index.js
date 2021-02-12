768e9c97a3ff7e2dcda4363a22900b3077956abf
sinamna <sina3050@gmail.com>
2021-02-12 11:57:28 +0330

    added handle click

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -4,7 +4,9 @@ import './index.css'
 class Square extends React.Component {
     render() {
       return (
-        <button className="square" onClick={()=> alert('clicked')}>
+        <button 
+          className="square" 
+          onClick={()=> this.props.onClick()}>
           {this.props.value}
         </button>
       );
@@ -12,8 +14,22 @@ class Square extends React.Component {
   }
   
   class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state={
+        squares: Array(9).fill(null),
+      }
+    }
+    handleClick(i){
+      const squares = this.state.squares.slice();
+      square[i]='X';
+      this.setState({squares:squares});
+    }
     renderSquare(i) {
-      return <Square value={i}/>
+      return (<Square 
+        value={this.state.squares[i]}
+        onClick={()=>this.handleClick(i)}
+        />);
     }
   
     render() {
