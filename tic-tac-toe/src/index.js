262215afa63e6397bd067bebd03a6a3f90041868
sinamna <sina3050@gmail.com>
2021-02-12 12:14:19 +0330

    completed base of game

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -23,6 +23,7 @@ class Square extends React.Component {
     }
     handleClick(i){
       const squares = this.state.squares.slice();
+      if(calculateWinner(squares)||squares[i])return;
       squares[i]=this.state.xIsNext?'X':'O';
       this.setState({
         squares:squares,
@@ -37,7 +38,13 @@ class Square extends React.Component {
     }
   
     render() {
-      const status = 'Next player: '+(this.state.xIsNext? 'X': 'O');
+      const haveWinner = calculateWinner(this.state.squares);
+      let status;
+      if(haveWinner){
+        status = `Winner ${haveWinner}`;
+      }else{
+        status = 'Next player: '+(this.state.xIsNext? 'X': 'O');
+      }
   
       return (
         <div>
@@ -77,7 +84,25 @@ class Square extends React.Component {
       );
     }
   }
-  
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
   // ========================================
   
   ReactDOM.render(
